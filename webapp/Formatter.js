e23e1b9b78ede3a88d07c1f19d4aeb6d54cf080c
estevan086 <estevan086@gmail.com>
2020-01-16 22:32:14 +0000

    Formmatter

--- webapp/Formatter.js
+++ webapp/Formatter.js
@@ -1,12 +1,31 @@
-jQuery.sap.declare("GridTable.util.Formatter");
-cbc.co.simulador_costos.Formatter={
- LinkDisplay:function(path){
-  if(path!==null){
-	  if(path==="R"){
-		return true;
-	  }else{
-		return false;
-	  }
-  }
- }
-};
+// jQuery.sap.declare("GridTable.util.Formatter");
+// cbc.co.simulador_costos.Formatter={
+//  LinkDisplay:function(path){
+//   if(path!==null){
+// 	  if(path==="R"){
+// 		return true;
+// 	  }else{
+// 		return false;
+// 	  }
+//   }
+//  }
+// };
+
+sap.ui.define([], function () {
+	"use strict";
+	return {
+		statusText: function (sStatus) {
+			var resourceBundle = this.getView().getModel("i18n").getResourceBundle();
+			switch (sStatus) {
+				case "A":
+					return resourceBundle.getText("invoiceStatusA");
+				case "B":
+					return resourceBundle.getText("invoiceStatusB");
+				case "C":
+					return resourceBundle.getText("invoiceStatusC");
+				default:
+					return sStatus;
+			}
+		}
+	};
+});
