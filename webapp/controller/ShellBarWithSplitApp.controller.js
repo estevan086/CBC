a8e256a428fb120d6da20072dea9458e3c6e01ff
Juan David Lopez Gutierrez <juandalop@gmail.com>
2020-01-22 22:09:57 +0000

    Navegación dinámica

--- webapp/controller/ShellBarWithSplitApp.controller.js
+++ webapp/controller/ShellBarWithSplitApp.controller.js
@@ -1,6 +1,6 @@
 sap.ui.define([
 	'sap/ui/Device',
-	'sap/ui/core/mvc/Controller',
+	'cbc/co/simulador_costos/controller/BaseController',
 	'sap/ui/model/json/JSONModel',
 	'sap/m/Popover',
 	'sap/m/Button',
@@ -9,196 +9,166 @@ sap.ui.define([
 	"use strict";
 	var ButtonType = mobileLibrary.ButtonType,
 		PlacementType = mobileLibrary.PlacementType;
-		
+
 	var CController = Controller.extend("cbc.co.simulador_costos.controller.ShellBarWithSplitApp", {
-		onInit : function() {
-			
+		onInit: function () {
+
 			// const router = this.getOwnerComponent().getRouter();
 			// router.attachRoutePatternMatched(this.onRoutePatternMatched.bind(this));
 			// this.getView().addEventDelegate({
 			// 	onAfterShow: this.onAfterShow.bind(this, router),
 			// });
-			
+
 			this.oModel = new JSONModel();
 			this.oModel.setData({
 				"selectedKey": "rtHome",
-				"navigation": [
-					{
-						"title": "Home",
-						"icon": "sap-icon://home",
-						"expanded": false,
-						"key": "rtHome"
-					},
-					{
-						"title": "Administración",
-						"icon": "sap-icon://employee",
-						"expanded": false,
-						"key": "rtAdministracion",
-						"items": [
-							{
-								"title": "Centros",
-								"key": "rtChCentros"
-							},
-							{
-								"title": "Unidad de Medida",
-								"key": "rtChUndMedida"
-							},
-							{
-								"title": "Periodos",
-								"key": "rtChPeriodo"
-							},
-							{
-								"title": "Logística",
-								"key": "rtChAdminLogistica"
-							},
-							{
-								"title": "Perfiles",
-								"key": "rtChUsers"
-							}
-						]
-					},
-					{
-						"title": "Datos Default",
-						"icon": "sap-icon://accounting-document-verification",
-						"expanded": false,
-						"key": "rtDataDefault",
-						"items": [
-							{
-								"title": "Mantenimiento Tipo Cambio",
-								"key": "rtChTypeChange"
-							},
-							{
-								"title": "Crear Icoterm",
-								"key": "rtChCreateIcoterm"
-							},
-							{
-								"title": "Copiar Versiones",
-								"key": "rtChCopyVersion"
-							},
-							{
-								"title": "Commodities",
-								"key": "rtChCommodities"
-							},
-							{
-								"title": "Materiales",
-								"key": "rtChMateriales"
-							},
-							{
-								"title": "Costos Logísticos",
-								"key": "rtChCostosLogisticos"
-							}
-						]
-					},
-					{
-						"title": "Escenarios",
-						"icon": "sap-icon://simulate",
-						"expanded": false,
-						"items": [
-							{
-								"title": "Matriz de Escenarios",
-								"key": "rtChMatrizEs"
-							},
-							//{
-							//	"title": "Control de Escenarios",
-							//	"key": "rtChControlEs"
-							//},
-							{
-								"title": "Administración Escenarios",
-								"key": "rtChAdmonEs"
-							}
-						]
-					},
-					{
+				"navigation": [{
+					"title": "Home",
+					"icon": "sap-icon://home",
+					"expanded": false,
+					"key": "rtHome"
+				}, {
+					"title": "Administración",
+					"icon": "sap-icon://employee",
+					"expanded": false,
+					"key": "rtAdministracion",
+					"items": [{
+						"title": "Centros",
+						"key": "rtChCentros"
+					}, {
+						"title": "Unidad de Medida",
+						"key": "rtChUndMedida"
+					}, {
+						"title": "Periodos",
+						"key": "rtChPeriodo"
+					}, {
+						"title": "Logística",
+						"key": "rtChAdminLogistica"
+					}, {
+						"title": "Perfiles",
+						"key": "rtChUsers"
+					}]
+				}, {
+					"title": "Datos Default",
+					"icon": "sap-icon://accounting-document-verification",
+					"expanded": false,
+					"key": "rtDataDefault",
+					"items": [{
+						"title": "Mantenimiento Tipo Cambio",
+						"key": "rtChTypeChange"
+					}, {
+						"title": "Crear Icoterm",
+						"key": "rtChCreateIcoterm"
+					}, {
+						"title": "Copiar Versiones",
+						"key": "rtChCopyVersion"
+					}, {
 						"title": "Commodities",
-						"icon": "sap-icon://factory",
-						"expanded": false,
-						"items": [
-							{
-								"title": "Mantenimiento Commodities",
-								"key": "rtChMantenimientoCommodities"
-							}
-						]
-					},
-					{
+						"key": "rtChCommodities"
+					}, {
 						"title": "Materiales",
-						"icon": "sap-icon://product",
-						"expanded": false,
-						"items": [
-							{
-								"title": "Mantenimiento Materiales",
-								"key": "rtChMantenimientoMateriales"
-							}
-						]
-					},
-					{
-						"title": "Costos Logístico",
-						"icon": "sap-icon://travel-expense",
-						"expanded": false,
-						"items": [
-							{
-								"title": "Mantenimiento Costos Logístico",
-								"key": "rtChMantenimientoCostLog"
-							}
-						]
-					},
-					{
-						"title": "COGS",
-						"icon": "sap-icon://settings",
-						"expanded": false,
-						"items": [
-							{
-								"title": "Reportes COGS",
-								"key": "rtCCOGS"
-							}
-						]
-					},
-					{
-						"title": "Reportes",
-						"icon": "sap-icon://pie-chart",
-						"expanded": false,
-						"items": [
-							{
-								"title": "Visualización",
-								"key": "rtChVisualizacion"
-							}
-						]
-					},
-					{
-						"title": "Manufactura",
-						"icon": "sap-icon://machine",
-						"expanded": false,
-						"items": [
-							{
-								"title": "Carga Manufactura",
-								"key": "rtChCargaManufactura"
-							}
-						]
-					}
+						"key": "rtChMateriales"
+					}, {
+						"title": "Costos Logísticos",
+						"key": "rtChCostosLogisticos"
+					}]
+				}, {
+					"title": "Escenarios",
+					"icon": "sap-icon://simulate",
+					"expanded": false,
+					"items": [{
+							"title": "Matriz de Escenarios",
+							"key": "rtChMatrizEs"
+						},
+						//{
+						//	"title": "Control de Escenarios",
+						//	"key": "rtChControlEs"
+						//},
+						{
+							"title": "Administración Escenarios",
+							"key": "rtChAdmonEs"
+						}
 					]
+				}, {
+					"title": "Commodities",
+					"icon": "sap-icon://factory",
+					"expanded": false,
+					"items": [{
+						"title": "Mantenimiento Commodities",
+						"key": "rtChMantenimientoCommodities"
+					}]
+				}, {
+					"title": "Materiales",
+					"icon": "sap-icon://product",
+					"expanded": false,
+					"items": [{
+						"title": "Mantenimiento Materiales",
+						"key": "rtChMantenimientoMateriales"
+					}]
+				}, {
+					"title": "Costos Logístico",
+					"icon": "sap-icon://travel-expense",
+					"expanded": false,
+					"items": [{
+						"title": "Mantenimiento Costos Logístico",
+						"key": "rtChMantenimientoCostLog"
+					}]
+				}, {
+					"title": "COGS",
+					"icon": "sap-icon://settings",
+					"expanded": false,
+					"items": [{
+						"title": "Reportes COGS",
+						"key": "rtCCOGS"
+					}]
+				}, {
+					"title": "Reportes",
+					"icon": "sap-icon://pie-chart",
+					"expanded": false,
+					"items": [{
+						"title": "Visualización",
+						"key": "rtChVisualizacion"
+					}]
+				}, {
+					"title": "Manufactura",
+					"icon": "sap-icon://machine",
+					"expanded": false,
+					"items": [{
+						"title": "Carga Manufactura",
+						"key": "rtChCargaManufactura"
+					}]
+				}]
 			});
 			//this.oModel.loadData(sap.ui.require.toUrl("sap/f/sample/ShellBarWithSplitApp/model") + "/model.json", null, false);
 			this.getView().setModel(this.oModel);
+			this.getRouter().navTo("home2");
 		},
 
-		onItemSelect : function(oEvent) {
-			
-			var router = this.getOwnerComponent().getRouter();
-			router.navTo(oEvent.getParameter("item").getProperty("key"));
-			
+		onItemSelect: function (oEvent) {
+
+			var router = this.getOwnerComponent().getRouter(),
+				sKey = oEvent.getParameter("item").getProperty("key");
+			//if (this._isPageInNavContainer(sKey)) {
+				router.navTo(sKey);
+			/*} else {
+				this._addPageToNavContainer(sKey);
+				router.navTo(sKey);
+			}*/
+
 			//var item = oEvent.getParameter('item');
 			//this.byId("NavContainer").to(this.getView().createId(item.getKey()));
 		},
 
-		onMenuButtonPress : function() {
+		onMenuButtonPress: function () {
 			var toolPage = this.byId("toolPage");
 			toolPage.setSideExpanded(!toolPage.getSideExpanded());
 		},
-		
+
 		handleUserNamePress: function (event) {
 			var popover = new Popover({
 				showHeader: false,
 				placement: PlacementType.Buttom,
-				content:[
+				content: [
 					new Button({
 						text: 'Feedback',
 						type: ButtonType.Transparent
@@ -216,8 +186,8 @@ sap.ui.define([
 
 			popover.openBy(event.getSource());
 		},
-		
-		onSideNavButtonPress : function() {
+
+		onSideNavButtonPress: function () {
 			var toolPage = this.byId("toolPage");
 			var sideExpanded = toolPage.getSideExpanded();
 
@@ -226,7 +196,7 @@ sap.ui.define([
 			toolPage.setSideExpanded(!toolPage.getSideExpanded());
 		},
 
-		_setToggleButtonTooltip : function(bLarge) {
+		_setToggleButtonTooltip: function (bLarge) {
 			var toggleButton = this.byId('sideNavigationToggleButton');
 			if (bLarge) {
 				toggleButton.setTooltip('Large Size Navigation');
@@ -234,24 +204,48 @@ sap.ui.define([
 				toggleButton.setTooltip('Small Size Navigation');
 			}
 		},
-		
-		onAfterNavigate : function(oEvent){
-		//	console.log("Test Navegacion");
+
+		onAfterNavigate: function (oEvent) {
+			//	console.log("Test Navegacion");
 		},
-		
-		handlePressConfiguration: function(oEvent) {
-		/*	var oItem = oEvent.getSource();
-			var oShell = this.byId("myShell");
-			var bState = oShell.getShowPane();
-			oShell.setShowPane(!bState);
-			oItem.setShowMarker(!bState);
-			oItem.setSelected(!bState);*/
+
+		handlePressConfiguration: function (oEvent) {
+			/*	var oItem = oEvent.getSource();
+				var oShell = this.byId("myShell");
+				var bState = oShell.getShowPane();
+				oShell.setShowPane(!bState);
+				oItem.setShowMarker(!bState);
+				oItem.setSelected(!bState);*/
 			var toolPage = this.byId("toolPage");
 			toolPage.setSideExpanded(!toolPage.getSideExpanded());
 		}
 
-	});
+		/*_isPageInNavContainer: function (sKey) {
+			var oNavContainer = this.byId("parentNavContainer"),
+				aPages = oNavContainer.getPages(),
+				sNewKey = this.getView().createId(sKey),
+				bResult = false;
+			aPages.forEach(oPage => {
+				if (oPage.getId() === sNewKey) {
+					bResult = true;
+					return false;
+				}
+			});
+			return bResult;
+		},
+		_addPageToNavContainer: function (sKey) {
+			let oScroll = new sap.m.ScrollContainer(this.getView().createId(sKey), {
+				horizontal: false,
+				vertical: true,
+				content: [new sap.ui.core.mvc.XMLView({
+					viewName: "cbc.co.simulador_costos.view.DataDefault.Commodities.AdminCommodities"
+				})]
+			});
+			let oNavContainer = this.byId("parentNavContainer");
+			oNavContainer.addPage(oScroll);
+		}*/
 
+	});
 
 	return CController;
 
