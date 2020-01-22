b2d7695610f4ad42eb5d0d11c472447fe4fc704e
kevin romero polanco <kevin.romero@onbusinessconsulting.com>
2020-01-22 05:47:45 +0000

    update IDCommodities

--- webapp/controller/DataDefault/Commodities/AdminIDCommodities.controller.js
+++ webapp/controller/DataDefault/Commodities/AdminIDCommodities.controller.js
@@ -6,39 +6,17 @@ sap.ui.define(["cbc/co/simulador_costos/controller/BaseController", "sap/ui/core
 
 	var that = this;
 	var MessageType = CoreLibrary.MessageType;
+	
 
 	return Controller.extend("cbc.co.simulador_costos.controller.DataDefault.Commodities.AdminIDCommodities", {
-	
-		
+
 		onInit: function () {
-			//	var sUrl = "#" + this.getOwnerComponent().getRouter().getURL("page1");
-			//	this.byId("link").setHref(sUrl);
 
-			// var sServiceUrl = this.getView().getParent().getParent().getModel("ModelSimulador").sServiceUrl,
-			// oModelService = new sap.ui.model.odata.ODataModel(sServiceUrl, true);
-			
-
-			// var oCreate = this.fnGetEntity(oModelService, "/headerCommoditiesSet");
-
-			// that = this;
-			// if (oCreate.tipo === "S") {
-			// 	if (oCreate.datos.Msj !== "" && oCreate.datos.Msj !== undefined) {
-			// 		sap.m.MessageToast.show(oCreate.datos.Msj);
-			// 	} else {
-
-			// 		this.getView().setModel(oCreate.datos);
-			// 	}
-
-			// } else {
-			// 	sap.m.MessageBox.error(oCreate.msjs, null, "Mensaje del sistema", "OK", null);
-			// }
-			
 		},
 
 		onAfterNavigate: function (oEvent) {
 			var sServiceUrl = this.getView().getParent().getParent().getModel("ModelSimulador").sServiceUrl,
-			oModelService = new sap.ui.model.odata.ODataModel(sServiceUrl, true);
-			
+				oModelService = new sap.ui.model.odata.ODataModel(sServiceUrl, true);
 
 			var oCreate = this.fnGetEntity(oModelService, "/headerCommoditiesSet");
 
@@ -54,26 +32,7 @@ sap.ui.define(["cbc/co/simulador_costos/controller/BaseController", "sap/ui/core
 			} else {
 				sap.m.MessageBox.error(oCreate.msjs, null, "Mensaje del sistema", "OK", null);
 			}
-			
-		},
-		
-		onToPage1: function (oEvent) {
 
-			var sPreviousHash = History.getInstance().getPreviousHash();
-
-			//The history contains a previous entry
-			if (sPreviousHash !== undefined) {
-				window.history.go(-1);
-			} else {
-				// There is no history!
-				// replace the current hash with page 1 (will not add an history entry)
-				this.getOwnerComponent().getRouter().navTo("rtChCommodities", null, true);
-			}
-
-			// var oApp = oEvent.getSource().getParent().getParent().getParent().getParent().getParent().getParent().getParent();
-			// var oNavContainer = oApp.byId("NavContainer");
-			// //oNavContainer.to(oApp.createId("rtChCommodities"));
-			// oNavContainer.back();
 		},
 
 		onBack: function () {
@@ -88,16 +47,34 @@ sap.ui.define(["cbc/co/simulador_costos/controller/BaseController", "sap/ui/core
 				this.getOwnerComponent().getRouter().navTo("rtChCommodities", null, true);
 			}
 		},
+		
+		saveCommodities: function (oEvent){
+			var sServiceUrl = this.getView().getModel("ModelSimulador").sServiceUrl,
+			oModelService = new sap.ui.model.odata.ODataModel(sServiceUrl, true),
+			oCommodities = [],
+			oEntidad = {},
+			oDetail = {};
+			
+			var oTable = this.byId("tblCommodities");
+			
+			for (var i = 1; i < JsonValue.length; i++) {
+				
+			}
+
+			var oSave = this.fnCreateEntity(oModelService, "/headerCommoditiesSet", oCommodities);
+			
+		},
 
 		showFormAddCommoditie: function (oEvent) {
 			//Abre Fragment para insertar registro de ID Commoditie
 			this.fnOpenDialog("cbc.co.simulador_costos.view.Utilities.fragments.AdminCommodities.AddCommodities");
 
 		},
+
 		AddCommoditie: function (oEvent) {
 
-			var sServiceUrl = this.getView().getModel("ModelSimulador").sServiceUrl,
-				oModelService = new sap.ui.model.odata.ODataModel(sServiceUrl, true);
+			/*	var sServiceUrl = this.getView().getModel("ModelSimulador").sServiceUrl,
+					oModelService = new sap.ui.model.odata.ODataModel(sServiceUrl, true);*/
 			var oEntidad = {};
 
 			var oIdCommoditie = sap.ui.getCore().getElementById("inputId").getValue();
@@ -106,7 +83,7 @@ sap.ui.define(["cbc/co/simulador_costos/controller/BaseController", "sap/ui/core
 			oEntidad.IdCommoditie = oIdCommoditie;
 			oEntidad.Descripcion = oDescCommoditie;
 
-			var oCreate = this.fnCreateEntity(oModelService, "/headerCommoditiesSet", oEntidad);
+			/*var oCreate = this.fnCreateEntity(oModelService, "/headerCommoditiesSet", oEntidad);
 
 			that = this;
 			if (oCreate.tipo === "S") {
@@ -116,16 +93,16 @@ sap.ui.define(["cbc/co/simulador_costos/controller/BaseController", "sap/ui/core
 
 					this.fnCloseFragment();
 
-					sap.m.MessageToast.show("ID Commoditie creada exitosamente.");
+					sap.m.MessageToast.show("ID Commoditie creada exitosamente.");*/
 
-					this.AddAllPeriodsforCommoditie(oEntidad);
+			this.AddAllPeriodsforCommoditie(oEntidad);
 
-				}
+			/*	}
 
 			} else {
 				sap.m.MessageBox.error(oCreate.msjs, null, "Mensaje del sistema", "OK", null);
-			}
-
+			}*/
+			this.fnCloseFragment();
 		},
 
 		AddAllPeriodsforCommoditie: function (oEntidad) {
@@ -133,55 +110,61 @@ sap.ui.define(["cbc/co/simulador_costos/controller/BaseController", "sap/ui/core
 			var today = new Date();
 			var year = today.getFullYear();
 
-			var oData = '{ "COMMODITIES" : [';
-			oData += '{ ';
+			if (!this.getView().getModel().getData().COMMODITIES) {
+				var oData = '{ "COMMODITIES" : [';
+				oData += '{ ';
 
-			oData += ' "CDEF_IDCOMMODITIES":"' + oEntidad.IdCommoditie + '", ';
-			oData += ' "CDEF_COMMODITIE":"' + oEntidad.Descripcion + '" ';
+				oData += ' "CDEF_IDCOMMODITIES":"' + oEntidad.IdCommoditie + '", ';
+				oData += ' "CDEF_COMMODITIE":"' + oEntidad.Descripcion + '" ';
 
-			oData = oData + '}]}';
-			var obj = $.parseJSON(oData);
+				oData = oData + '}]}';
+				var obj = $.parseJSON(oData);
 
-			var json = new sap.ui.model.json.JSONModel(obj);
-			this.getView().setModel(json);
+				var json = new sap.ui.model.json.JSONModel(obj);
+				this.getView().setModel(json);
 
-			oModel.setData(oData);
+				oModel.setData(oData);
+				var fnEditDetail = this.handleEditPress.bind(this);
+				var fnDleteDetail = this.handleDeletePress.bind(this);
+				this.modes = [{
+					key: "NavigationDelete",
+					text: "Navigation & Delete",
+					handler: function () {
+						var oTemplate = new sap.ui.table.RowAction({
+							items: [
+								new sap.ui.table.RowActionItem({
+									icon: "sap-icon://edit",
+									text: "Edit",
+									press: fnEditDetail
+								}),
+								new sap.ui.table.RowActionItem({
+									icon: "sap-icon://delete",
+									text: "Eliminar",
+									press: fnDleteDetail,
+									id: "btnFormuladora"
+								})
+							]
+						});
+						return [2, oTemplate];
+					}
+				}];
+				this.getView().setModel(new JSONModel({
+					items: this.modes
+				}), "modes");
+				this.switchState("NavigationDelete");
+				return oModel;
+			} else {
+				var oData = '{ ';
 
-		/*	var fnEditDetail = this.showFormEditDetail.bind(this);
+				oData += ' "CDEF_IDCOMMODITIES":"' + oEntidad.IdCommoditie + '", ';
+				oData += ' "CDEF_COMMODITIE":"' + oEntidad.Descripcion + '" ';
 
-			this.modes = [{
-				key: "NavigationDelete",
-				text: "Navigation & Delete",
-				handler: function () {
-					var oTemplate = new sap.ui.table.RowAction({
-						items: [
-							new sap.ui.table.RowActionItem({
-								icon: "sap-icon://edit",
-								text: "Edit",
-								press: fnEditDetail
-							})
-						]
-					});
-					return [1, oTemplate];
-				}
-			}];
-			this.getView().setModel(new JSONModel({
-				items: this.modes
-			}), "modes");
-			this.switchState("NavigationDelete");*/
+				oData = oData + '}';
+				var obj = $.parseJSON(oData);
+				this.getView().getModel().getData().COMMODITIES.push(obj);
+				this.getView().getModel().refresh();
+			}
 
-			return oModel;
-		},
-
-		showFormEditDetail: function (oEvent) {
-			/*	this.LogisticaDisplay = sap.ui.xmlfragment(
-					"cbc.co.simulador_costos.view.Utilities.fragments.AdminCommodities.EditDetailCommodities", this);
-				this.LogisticaDisplay.open();*/
-			//this.getOwnerComponent().OpnFrmLogitica();
-		},
-
-		closeDialog: function (oEvent) {
-			this.fnCloseFragment();
 		},
 
 		switchState: function (sKey) {
@@ -206,6 +189,10 @@ sap.ui.define(["cbc/co/simulador_costos/controller/BaseController", "sap/ui/core
 			oTable.setRowActionCount(iCount);
 		},
 
+		closeDialog: function (oEvent) {
+			this.fnCloseFragment();
+		},
+
 		handleEditPress: function (oEvent, Data) {
 			//var oRow = oEvent.getParameter("row");
 			var oItem = oEvent.getParameter("item");
@@ -217,15 +204,18 @@ sap.ui.define(["cbc/co/simulador_costos/controller/BaseController", "sap/ui/core
 
 			for (var i = 0; i < oTable.getRows().length; i++) {
 
-				oTable.getRows()[i].getBindingContext().getProperty().CDEF_EDIT_FLAG = "None";
-				oTable.getRows()[i].getBindingContext().getProperty().CDEF_NAV_FLAG = false;
+				if (oTable.getRows()[i].getBindingContext() !== null) {
 
-				//Descripcion
-				oTable.getRows()[i].getCells()[1].setProperty("editable", false);
+					oTable.getRows()[i].getBindingContext().getProperty().CDEF_EDIT_FLAG = "None";
+					oTable.getRows()[i].getBindingContext().getProperty().CDEF_NAV_FLAG = false;
+
+					//Descripcion
+					oTable.getRows()[i].getCells()[1].setProperty("editable", false);
+				}
 			}
 
 			//Descripcion
-			oRowEdited.getCells()[2].setProperty("editable", true);
+			oRowEdited.getCells()[1].setProperty("editable", true);
 
 			oRowData.CDEF_EDIT_FLAG = "Information";
 
@@ -237,6 +227,22 @@ sap.ui.define(["cbc/co/simulador_costos/controller/BaseController", "sap/ui/core
 
 			}));
 
+			sap.ui.getCore().applyChanges();
+
+			MessageToast.show("ID " + (oItem.getText() || oItem.getType()) + " pressed for id " + oRowData.CDEF_IDCOMMODITIES);
+			
+		},
+
+		handleDeletePress: function (oEvent, Data) {
+			//var oRow = oEvent.getParameter("row");
+			var oItem = oEvent.getParameter("item");
+
+			var oTable = this.byId("tblCommodities");
+			var oRowData = oEvent.getSource().getBindingContext().getProperty();
+
+			var oRowEdited = oEvent.getSource().getParent().getParent();
+
+			
 			sap.ui.getCore().applyChanges();
 
 			MessageToast.show("ID " + (oItem.getText() || oItem.getType()) + " pressed for id " + oRowData.CDEF_IDCOMMODITIES);
