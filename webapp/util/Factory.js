9e246de307f4e3543dd3fc31b45d6c5f8dd21529
Juan David Lopez Gutierrez <juandalop@gmail.com>
2020-02-01 04:10:38 +0000

    Ajuste versiones

---
+++ webapp/util/Factory.js
@@ -0,0 +1,29 @@
+sap.ui.define([], function () {
+	"use strict";
+	return {
+		factoryColumnsScenes: function (sId, oContext) {
+			var oObject;
+			if (oContext.getObject().id === "00") {
+				oObject = new sap.ui.table.Column({
+					label: this.getResourceBundle().getText("moduleScenarioViewHeader"),
+					template: new sap.m.Text({
+						text: "{viewModel>modulo}"
+					})
+				});
+			} else {
+				oObject = new sap.ui.table.Column({
+					label: oContext.getObject().value,
+					template: new sap.m.Input({
+						value: "{viewModel>" + oContext.getObject().id + "}",
+						showValueHelp: true,
+						valueHelpOnly: true
+					}).attachValueHelpRequest(function(oEvent){
+						this._oSelectDialog = this._createSelectDialogOriginVersion(oEvent.getSource().data("row"), oEvent.getSource().data("mes"));
+						this._oSelectDialog.open();
+					}.bind(this)).data({"mes": oContext.getObject().id, "row": "{viewModel>tipoVersion}"})
+				});
+			}
+			return oObject;
+		}	
+	};
+});
